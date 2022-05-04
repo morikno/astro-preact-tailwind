ca5600da3b789bc7b6d352e6f4b0dbdf1f596eb6
morishita <morishita@medialab.co.jp>
2022-05-04 15:58:04 +0900

    re:first_commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), react(), tailwind()]
+});
